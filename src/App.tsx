b789d642827f4111df2cd7660b88c708d27e8e05
Lev Kowalski <levkowalski@gmail.com>
2022-09-20 22:09:53 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
-import styled from "@emotion/styled";
 import { TreeNodeDisplay } from "./TreeNode";
 import { make } from "./binarySearchTree";
-
-const Box = styled.div({
-  display: "flex",
-  justifyContent: "space-around",
-  alignItems: "center",
-});
 
 function App() {
   const root = make([8, 1, 3, 30, 20, 79, 2, 4, 11, 6, 9, 5, 7]);

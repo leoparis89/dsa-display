b7f5ebbf4d1352e3cd0cf7c1ec099225cc0e023d
Lev Kowalski <levkowalski@gmail.com>
2022-09-20 13:55:56 +0200

    wip

---
+++ src/binarySearchTree.ts
@@ -0,0 +1,31 @@
+export class TreeNode {
+  value: number;
+  left: TreeNode | null = null;
+  right: TreeNode | null = null;
+  constructor(value: number) {
+    this.value = value;
+  }
+}
+
+type addType = (value: number, node: TreeNode) => TreeNode;
+// typescript not undestanding the return type
+// so we have to force it (awsome)
+export const add = ((value, node) => {
+  if (value < node.value) {
+    if (!node.left) {
+      node.left = new TreeNode(value);
+      return node.left;
+    } else {
+      return add(value, node.left);
+    }
+  }
+
+  if (value >= node.value) {
+    if (!node.right) {
+      node.right = new TreeNode(value);
+      return node.right;
+    } else {
+      return add(value, node.right);
+    }
+  }
+}) as addType;

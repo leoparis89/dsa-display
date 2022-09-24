71dc96f20d9f279ded8777fccfe0f3bbaaff7ffb
Lev Kowalski <levkowalski@gmail.com>
2022-09-24 21:58:57 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 
 // import { TreeNodeDisplay } from "./tree/TreeNode";
 // import { make } from "./tree/binarySearchTree";
-import { BinaryTree } from "./tree/BinaryTree";
+import { BinaryTree } from "./tree/BinaryTreeScreen";
 
 function App() {
   // const root = make([8, 1, 3, 30, 20, 79, 2, 4, 11, 6, 9, 5, 7, 200, 150, 300]);

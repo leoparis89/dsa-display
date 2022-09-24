9ef2e15f632ce7e63cb312f0aa2a155dfa4e31e9
Lev Kowalski <levkowalski@gmail.com>
2022-09-24 15:28:06 +0200

    wip

--- src/tree/BinaryTree.tsx
+++ src/tree/BinaryTree.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { TreeNode, add } from "./binarySearchTree";
 
 export const BinaryTree = () => {
-  let [tree, setTree] = React.useState(null);
+  let [tree, setTree] = React.useState<TreeNode | null>(null);
   let [val, setVal] = React.useState<number>();
 
+  const handleClickAdd = (val: number) => {
+    if (!tree) {
+      setTree(new TreeNode(val));
+    } else {
+      add(val, tree);
+    }
+  };
+
+  console.log(tree);
   return (
     <div>
       <input
@@ -11,13 +21,17 @@ export const BinaryTree = () => {
         onChange={(e) => {
           const val = Number(e.target.value);
           setVal(val);
-          console.log(val);
         }}
         type="number"
         aria-label="value"
         placeholder="value"
       />
-      <button>Add</button>
+      <button
+        disabled={val === undefined}
+        onClick={(_) => handleClickAdd(val!)}
+      >
+        Add
+      </button>
       <button onClick={(_) => setTree(null)}>Reset</button>
       hello
     </div>

e530fdf08b8a9c602bd1ee16206e79b93822a2d2
Lev Kowalski <levkowalski@gmail.com>
2022-09-24 12:46:08 +0200

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
-import { TreeNodeDisplay } from "./tree/TreeNode";
-import { make } from "./tree/binarySearchTree";
+// import { TreeNodeDisplay } from "./tree/TreeNode";
+// import { make } from "./tree/binarySearchTree";
+import { BinaryTree } from "./tree/BinaryTree";
 
 function App() {
-  const root = make([8, 1, 3, 30, 20, 79, 2, 4, 11, 6, 9, 5, 7, 200, 150, 300]);
+  // const root = make([8, 1, 3, 30, 20, 79, 2, 4, 11, 6, 9, 5, 7, 200, 150, 300]);
   // const levels = levelOrderTraversalWithNulls(tree);
   return (
-    <div>
-      <TreeNodeDisplay node={root} />
-      {/* {levels.map((l) => (
-        <Level nodeValues={l} />
-      ))} */}
-    </div>
+    <>
+      <Routes>
+        <Route index={false} path="*" element={<BinaryTree />} />
+      </Routes>
+    </>
   );
 }
 

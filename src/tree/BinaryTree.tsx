a7feea802e8e812dcb604ae2abe251d50cdae5df
Lev Kowalski <levkowalski@gmail.com>
2022-09-24 15:02:39 +0200

    wip

--- src/tree/BinaryTree.tsx
+++ src/tree/BinaryTree.tsx
@@ -1,5 +1,25 @@
 import React from "react";
 
 export const BinaryTree = () => {
-  return <div>hello</div>;
+  let [tree, setTree] = React.useState(null);
+  let [val, setVal] = React.useState<number>();
+
+  return (
+    <div>
+      <input
+        value={val || ""}
+        onChange={(e) => {
+          const val = Number(e.target.value);
+          setVal(val);
+          console.log(val);
+        }}
+        type="number"
+        aria-label="value"
+        placeholder="value"
+      />
+      <button>Add</button>
+      <button>Reset</button>
+      hello
+    </div>
+  );
 };
